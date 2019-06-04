dd73f5e64997069a512e3669572f64610a1f72e6
tess.olson <tess.olson@T3600-CRUSHER>
2019-06-04 17:18:11 -0500

    added script and test light

---
+++ Assets/Scripts/CameraFlash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlash : MonoBehaviour
+{
+    //the intensity of the camera flash
+    private float flashIntensity;
+    //the light object for the flash
+    public Light flashObject;
+
+    //the amount of time the flash lasts
+    public float flashExposureTime = 2;
+
+    //amount of clicks needed to wind
+    public float cameraWindClicks = 5;
+
+    //flash cool down
+    public float flashWindDown = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
