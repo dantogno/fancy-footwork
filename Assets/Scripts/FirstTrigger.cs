a685c8a92129c33d489c2f848d8edc2853898ac3
katkeller <kat.kek.keller@gmail.com>
2019-06-12 16:31:24 -0500

    Fixed bug where footstep path didn't calculate, added door model

---
+++ Assets/Scripts/FirstTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTrigger : MonoBehaviour
+{
+    //[SerializeField]
+    private MeshCollider triggerCollider;
+    private bool hasBeenTriggered = false;
+
+    private void Awake()
+    {
+        triggerCollider = GetComponent<MeshCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !hasBeenTriggered)
+        {
+            Pathfinder.calculatePath = true;
+            hasBeenTriggered = true;
+            Debug.Log("First trigger has been triggered.");
+        }
+    }
+}
