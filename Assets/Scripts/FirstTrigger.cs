7a039387bc5cb3e5d0c328576ac31a590c26e484
Kat Keller <kat.kek.keller@gmail.com>
2019-06-12 20:09:55 -0500

    Added the appearence of a ghost figure to the first triggered interaction

--- Assets/Scripts/FirstTrigger.cs
+++ Assets/Scripts/FirstTrigger.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class FirstTrigger : MonoBehaviour
 {
     [SerializeField]
-    private Light light1, light2, spotLight;
+    private Light light1, light2, backLight, spotLight;
 
     [SerializeField]
     private AudioClip lightsOff, lightsOn;
 
     [SerializeField]
-    private float delay1 = 1.0f, delay2 = 1.0f;
+    private float delay1 = 1.0f, delay2 = 1.0f, stutterDelay = 0.1f;
 
     [SerializeField]
-    private GameObject objectToActivate;
+    private GameObject objectToActivate, ghostFigure;
 
     private MeshCollider triggerCollider;
     private bool hasBeenTriggered = false;
@@ -44,9 +44,23 @@ public class FirstTrigger : MonoBehaviour
         light2.intensity = 0.0f;
         audioSource.PlayOneShot(lightsOff, 1.0f);
         yield return new WaitForSeconds(delay1);
-        objectToActivate.SetActive(true);
-        spotLight.intensity = 3.5f;
+        ghostFigure.SetActive(true);
         audioSource.PlayOneShot(lightsOn, 1.0f);
-
+        backLight.intensity = 1.5f;
+        yield return new WaitForSeconds(stutterDelay);
+        audioSource.PlayOneShot(lightsOff, 1.0f);
+        backLight.intensity = 0.0f;
+        yield return new WaitForSeconds(stutterDelay);
+        backLight.intensity = 1.5f;
+        audioSource.PlayOneShot(lightsOn, 1.0f);
+        objectToActivate.SetActive(true);
+        yield return new WaitForSeconds(stutterDelay);
+        audioSource.PlayOneShot(lightsOff, 1.0f);
+        backLight.intensity = 0.0f;
+        ghostFigure.SetActive(false);
+        yield return new WaitForSeconds(delay2);
+        spotLight.intensity = 3.0f;
+        light1.intensity = 0.2f;
+        audioSource.PlayOneShot(lightsOn, 1.0f);
     }
 }
