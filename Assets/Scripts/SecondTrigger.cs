b86811c79f1eced5ebb0c8edc584c454112128a8
Kat Keller <kat.kek.keller@gmail.com>
2019-06-15 19:15:55 -0500

    Added script/trigger that turns off the lights the the first hall while turning on the lights in the second hall

---
+++ Assets/Scripts/SecondTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SecondTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Light[] firstHallLights = new Light[6];
+
+    [SerializeField]
+    private Light[] secondHallLights = new Light[8];
+
+    private float[] secondHallLightsIntensity = new float[8];
+    private MeshCollider triggerCollider;
+    private bool hasBeenTriggered = false;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        triggerCollider = GetComponent<MeshCollider>();
+        audioSource = GetComponent<AudioSource>();
+        GetLightIntensity();
+    }
+
+    private void GetLightIntensity()
+    {
+        for (int k = 0; k < 8; k++)
+        {
+            secondHallLightsIntensity[k] = secondHallLights[k].intensity;
+            secondHallLights[k].intensity = 0.0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !hasBeenTriggered)
+        {
+            TurnLights();
+            hasBeenTriggered = true;
+        }
+    }
+
+    private void TurnLights()
+    {
+        audioSource.Play();
+
+        for (int i = 0; i < 6; i++)
+        {
+            firstHallLights[i].intensity = 0.0f;
+        }
+
+        for (int j = 0; j < 8; j++)
+        {
+            secondHallLights[j].intensity = secondHallLightsIntensity[j];
+        }
+    }
+}
