0d43ae9bbb7bf7b20afde9104070141d17892666
Tess Olson <37564977+eleanorpixie2@users.noreply.github.com>
2019-06-16 23:35:35 -0500

    Added a breathing idle to the player camera

---
+++ Assets/Scripts/Breathing_HeadBob.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathing_HeadBob : MonoBehaviour
+{
+    Vector3 startPos;
+    public float amplitude = 10f;
+    public float period = 5f;
+    private GameObject player;
+    private Vector3 lastPosition = new Vector3(0, 0, 0);
+    protected void Start()
+    {
+        startPos = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    protected void Update()
+    {
+        if (player.transform.position==lastPosition)
+        {
+            float theta = Time.timeSinceLevelLoad / period;
+            float distance = amplitude * Mathf.Sin(theta);
+            transform.position = player.transform.position + Vector3.up * distance;
+        }
+        lastPosition = player.transform.position;
+    }
+}
