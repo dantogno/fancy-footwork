6124dab324a45302956491717572613ef9556c58
BagboBillbins <aaron.baer@loop.colum.edu>
2019-06-19 00:28:18 -0500

    ypdated the phone prefab to have the right code, added handprint prefabs with sound, and a script to control the handprint sound

---
+++ Assets/Scripts/HandPrintSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPrintSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip laugh;
+
+    private AudioSource audioSource;
+    private BoxCollider triggerCollider;
+    private bool hasBeenTriggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        triggerCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !hasBeenTriggered)
+        {
+            audioSource.PlayOneShot(laugh);
+            hasBeenTriggered = true;
+        }
+    }
+}
