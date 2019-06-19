92e3818b8342a0cfa2a7e781665e911d3686d0fd
tess.olson <tess.olson@T3600-CRUSHER>
2019-06-19 17:09:36 -0500

    ui is done

---
+++ Assets/Scripts/Collision_sight/NoteUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteUI : MonoBehaviour
+{
+    Interactable interact;
+    public Image UI;
+    private bool clickedButton=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interact = GetComponent<Interactable>();
+        UI.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interact.lookingAt && !clickedButton)
+        {
+            UI.enabled = true;
+        }
+        else if(!interact.lookingAt && !clickedButton)
+        {
+            UI.enabled = false;
+        }
+        if(interact.triggered)
+        {
+            UI.enabled = false;
+            clickedButton = true;
+        }
+    }
+}
